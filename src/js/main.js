17ef660a033ede65435fa320be866110f92b9562
ViktoriiaYakaba <yakaba@bk.ru>
2024-01-12 17:27:56 +0100

    summary

---
+++ src/js/main.js
@@ -0,0 +1,76 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector(".gallery");
+const container = document.querySelector("div");
+const inputDate = document.querySelector("input");
+const btn = document.querySelector(".btn");
+
+
+const showLoader = () => {
+  const loader = document.createElement('span');
+  loader.classList.add('loader');
+  container.append(loader);
+};
+
+btn.addEventListener("submit", (event) => {
+   showLoader();
+   gallery.innerHTML = "";
+   event.preventDefault();
+   const searchTerm = inputDate.value;
+   searchImages(searchTerm);
+});
+
+function searchImages(searchTerm) {
+  const apiKey = '41764579-b97d65b31c0abd4efd9d4830e';
+  const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  fetch(url)
+     .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            } else {
+                return response.json();
+            }
+        })
+    .then(data => {
+        if (data.hits.length === 0) {
+            iziToast.error({
+                title: 'Error',
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                position: 'topRight'
+            });
+        } else {
+       const markup = data.hits
+            .map(data => {
+            return `<li class="gallery-item"><a href="${data.webformatURL}">
+          <img class="gallery-image" src="${data.largeImageURL}" alt="${data.tags}"></a>
+          <p><b>Likes: </b>${data.likes}</p>
+          <p><b>Views: </b>${data.views}</p>
+          <p><b>Comments: </b>${data.comments}</p>
+          <p><b>Downloads: </b>${data.downloads}</p>
+          </li>`;
+            }).join('');
+            
+            gallery.insertAdjacentHTML("beforeend", marcup);
+        const lightbox = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionType: 'attr',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        fadeSpeed: 150,
+        captionSelector: "img",
+        captionDelay: 250,
+       });
+
+        lightbox.on('show.simplelightbox').refresh();
+                   
+ 
+        }
+         })
+      
+    .catch((error) => console.log(error));
+}
+
